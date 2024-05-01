72010b1ab9fe37b67446a8e88395721fb2208e46
alexandraon <alexandra.onciul@student.uclouvain.be>
2024-05-01 10:53:48 +0200

    debut frontend

--- front-end/main.py
+++ front-end/main.py
@@ -2,9 +2,13 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def home():
+    if request.method == "POST":
+        return render_template("index.html") 
     return render_template("index.html")
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
